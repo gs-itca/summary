e92a64cd9f6713e90d928edc147dbb82bbc285e0
yuguang <yuguang@gaoding.com>
2020-06-29 10:47:42 +0800

    Updates

--- Js内功/this.js
+++ Js内功/this.js
@@ -1,36 +1,12 @@
-/*
- * @desc:
- * @Author: 余光
- * @Email: webbj97@163.com
- * @Date: 2020-05-12 15:22:10
- */
+var scope = "global scope";
+function checkscope(){
+    var scope = "local scope";
+    function f(){
+        return scope;
+    }
+    return f;
+}
 
-// var i = function() {
-//     console.log('余光')
-// }();
-
-// true && function() {
-//     console.log('余光')
-// }();
-
-// 0, function() { console.log('余光') }();
-
-
-void function() {
-    console.log('余光');
-}();
-
-new function() {
-    console.log('余光');
-}();
-
-var currentTime = (function () {
-    var time = new Date();
-    var year  = time.getFullYear()
-    var month = time.getMonth()+1;
-    var date  = time.getDate();
-    var hour  = time.getHours();
-    var min   = time.getMinutes();
-    return year + '-' + month + '-' + date + ' ' + hour + ':' + min;
-})()
-console.log('currentTime:', currentTime);
+var foo = checkscope();
+foo();
+console.log('foo()', foo());
