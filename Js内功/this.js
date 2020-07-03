3566ead85d286a91ce2d5051eaecd4b7bd70f9cc
yuguang <yuguang@gaoding.com>
2020-07-03 14:34:19 +0800

    Updates

--- Js内功/this.js
+++ Js内功/this.js
@@ -1,3 +1,9 @@
+/*
+ * @desc:
+ * @Author: 余光
+ * @Email: webbj97@163.com
+ * @Date: 2020-05-12 15:22:10
+ */
 var scope = "global scope";
 function checkscope(){
     var scope = "local scope";
@@ -10,3 +16,13 @@ function checkscope(){
 var foo = checkscope();
 foo();
 console.log('foo()', foo());
+
+
+var group = {
+    num: 10
+}
+function func(obj) {
+    obj.num += 10;
+    console.log('obj:', obj);
+}
+func(group); // { num: 20 }
