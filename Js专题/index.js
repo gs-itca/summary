97eba37f10d340e57a76951240c2ab52eb4c4f4a
yuguang <yuguang@gaoding.com>
2020-07-16 19:20:13 +0800

    Updates

--- Js专题/index.js
+++ Js专题/index.js
@@ -1,11 +1,8 @@
-var reg = /\w+\.(png|jpg|jpeg|psd)/
-var str1 = 'test.jpg'
-var str2 = 'test.jpeg'
-var str3 = 'test.aaa.psd'
-var str4 = 'test..png'
-var str5 = 'test..aaa'
-console.log(reg.test(str1));
-console.log(reg.test(str2));
-console.log(reg.test(str3));
-console.log(reg.test(str4));
-console.log(reg.test(str5));
+/*
+ * @desc:
+ * @Author: 余光
+ * @Email: webbj97@163.com
+ * @Date: 2019-11-22 10:20:48
+ */
+var str = '123';
+console.log(str); //
