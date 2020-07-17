efcc600976311713bbee9bf46aed35ab224f7a5c
yuguang <yuguang@gaoding.com>
2020-07-17 09:35:58 +0800

    Updates

--- node/csdn-data.js
+++ node/csdn-data.js
@@ -1,250 +1,208 @@
-/*
- * @desc:
- * @Author: 余光
- * @Email: webbj97@163.com
- * @Date: 2020-07-10 15:05:41
- */
-const LIST = [
+[
 	{
-		"titie": "JavaScript中的基本数据类型，地基同样重要（系列九）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/107151991",
-		"visit": "2357",
-		"discuss": "26"
-	},
-	{
-		"titie": "JavaScript中的参数传递(求值策略)，ECMAScript中所有函数的参数都是按值传递吗（系列八）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/107111529",
-		"visit": "5319",
-		"discuss": "10"
-	},
-	{
-		"titie": "JavaScript之闭包，给自己的Js一场重生（系列七）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106940646",
-		"visit": "6259",
-		"discuss": "12"
-	},
-	{
-		"titie": "JavaScript之深入理解立即调用函数表达式（IIFE），你对它的理解，决定了它的出镜率（系列六）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106824296",
-		"visit": "8710",
-		"discuss": "52"
-	},
-	{
-		"titie": "JavaScript中的变量对象，简约却不简单（系列五）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106793254",
-		"visit": "3765",
-		"discuss": "32"
-	},
-	{
-		"titie": "JavaScript中的执行上下文，既然遇见了这篇图文并茂的文章，干脆看完吧！（系列四）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106668345",
-		"visit": "5348",
-		"discuss": "28"
-	},
-	{
-		"titie": "从作用域到作用域链，思维脑图+代码示例让知识点一目了然！系列（三）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106618708",
-		"visit": "4503",
-		"discuss": "21"
-	},
-	{
-		"titie": "从原型到原型链，修炼JavaScript内功这篇文章真的不能错过！系列（二）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106555301",
-		"visit": "3687",
-		"discuss": "25"
-	},
-	{
-		"titie": "this指向详解，思维脑图与代码的结合，让你一篇搞懂this、call、apply。系列（一）",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106479511",
-		"visit": "4327",
-		"discuss": "10"
-	},
-	{
-		"titie": "算法与数据结构+LeetCode题解-Js版",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/103808459",
-		"visit": "568",
-		"discuss": "0"
-	},
-	{
-		"titie": "前端进阶笔记：记录成长",
 		"link": "https://blog.csdn.net/jbj6568839z/article/details/103161970",
-		"visit": "251",
-		"discuss": "0"
+		"title": "前端进阶笔记：记录成长"
 	},
 	{
-		"titie": "LeetCode题解：19.删除链表的倒数第N个节点",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106385853",
-		"visit": "859",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/107259293",
+		"title": "你真的了解Js吗？用五个问题来回顾之前的内容，留下你的答案我们一起进步！（系列十）"
 	},
 	{
-		"titie": "LeetCode题解：520.检测大写字母",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106265191",
-		"visit": "636",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/107151991",
+		"title": "JavaScript中的基本数据类型，地基同样重要（系列九）"
 	},
 	{
-		"titie": "LeetCode题解：451.根据字符出现频率排序",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106240845",
-		"visit": "459",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/107111529",
+		"title": "JavaScript中的参数传递(求值策略)，ECMAScript中所有函数的参数都是按值传递吗（系列八）"
 	},
 	{
-		"titie": "LeetCode题解：387.字符串中的第一个唯一字符",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106238739",
-		"visit": "336",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/106940646",
+		"title": "JavaScript之闭包，给自己的Js一场重生（系列七）"
 	},
 	{
-		"titie": "LeetCode题解：389.找不同",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106236988",
-		"visit": "215",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/106824296",
+		"title": "JavaScript之深入理解立即调用函数表达式（IIFE），你对它的理解，决定了它的出镜率（系列六）"
 	},
 	{
-		"titie": "LeetCode题解：374.猜数字大小",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106208957",
-		"visit": "62",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/106793254",
+		"title": "JavaScript中的变量对象，简约却不简单（系列五）"
 	},
 	{
-		"titie": "LeetCode题解：137.只出现一次的数字II",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106189353",
-		"visit": "71",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/106618708",
+		"title": "从作用域到作用域链，思维脑图+代码示例让知识点一目了然！系列（三）"
 	},
 	{
-		"titie": "LeetCode题解：61.旋转链表",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106059857",
-		"visit": "60",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/106668345",
+		"title": "JavaScript中的执行上下文，既然遇见了这篇图文并茂的文章，干脆看完吧！（系列四）"
 	},
 	{
-		"titie": "LeetCode题解：430.扁平化多级双向链表",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/106055733",
-		"visit": "91",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/106555301",
+		"title": "从原型到原型链，修炼JavaScript内功这篇文章真的不能错过！系列（二）"
 	},
 	{
-		"titie": "别说链表不重要（四）：双链表的添加删除基本原理+Js实现一个双链表类",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105999389",
-		"visit": "72",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/106479511",
+		"title": "this指向详解，思维脑图与代码的结合，让你一篇搞懂this、call、apply。系列（一）"
+	}
+],
+[
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104990375",
+		"title": "LeetCode每日一题：最小的k个数（七十八）"
 	},
 	{
-		"titie": "别说链表不重要（三）：链表高频面试题、经典问题一篇搞定",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105966404",
-		"visit": "82",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104960225",
+		"title": "LeetCode每日一题：836.矩形重叠（七十七）"
 	},
 	{
-		"titie": "LeetCode题解：234.回文链表",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105976157",
-		"visit": "67",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104915221",
+		"title": "LeetCode题解=>443.压缩字符串（七十六）"
 	},
 	{
-		"titie": "LeetCode题解：328.奇偶链表",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105972870",
-		"visit": "52",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104891901",
+		"title": "LeetCode每日一题：字符串压缩（七十五）"
 	},
 	{
-		"titie": "别说链表不重要（二）：双指针巧解链表经典问题，环形链表、相交链表",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105945871",
-		"visit": "94",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104754068",
+		"title": "算法与数据结构——字典"
 	},
 	{
-		"titie": "LeetCode题解：160.相交链表",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105955671",
-		"visit": "77",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104698486",
+		"title": "如何优雅的解决斐波那契数列"
 	},
 	{
-		"titie": "LeetCode题解：142.环形链表II",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105953898",
-		"visit": "79",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104061773",
+		"title": "LeetCode题解=>260.只出现一次的数字III（七十五）"
 	},
 	{
-		"titie": "别说链表不重要（一）：单链表的实现原理+JavaScript实现实战+常见操作一篇搞定",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105855889",
-		"visit": "90",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104036988",
+		"title": "LeetCode题解=>498.对角线遍历（七十四）"
 	},
 	{
-		"titie": "LeetCode题解：707.设计链表",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105840330",
-		"visit": "64",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104018682",
+		"title": "LeetCode题解=>66.加一（七十三）"
 	},
 	{
-		"titie": "LeetCode题解：504.七进制数",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105790806",
-		"visit": "69",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104017993",
+		"title": "LeetCode题解=>747.至少是其他数字两倍的最大数（七十二）"
 	},
 	{
-		"titie": "LeetCode题解：1389.按既定顺序创建目标数组",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105770848",
-		"visit": "79",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/104017023",
+		"title": "Leetcode题解=>724.寻找数组的中心索引（七十一）"
 	},
 	{
-		"titie": "LeetCode题解：1299.将每个元素替换为右侧最大元素",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105770684",
-		"visit": "70",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103993031",
+		"title": "Leetcode题解=>977.有序数组的平方（七十）"
 	},
 	{
-		"titie": "LeetCode题解：912.排序数组",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105769755",
-		"visit": "67",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103986774",
+		"title": "LeetCode题解=>976.三角形的最大周长（六十九）"
 	},
 	{
-		"titie": "LeetCode题解：896.单调数列",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105768637",
-		"visit": "56",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103984185",
+		"title": "LeetCode题解=>922.按奇偶排序数组II（六十八）"
 	},
 	{
-		"titie": "LeetCode题解：703.数据流中的第K大元素",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105763926",
-		"visit": "84",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103983479",
+		"title": "LeetCode题解=>905.按奇偶排序数组（六十七）"
 	},
 	{
-		"titie": "Leetcode题解=>627.交换工资",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105736934",
-		"visit": "59",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103977746",
+		"title": "LeetCode题解（博客）自我规范"
 	},
 	{
-		"titie": "LeetCode题解=>443.压缩字符串",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105736734",
-		"visit": "71",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103977622",
+		"title": "LeetCode题解=>860.柠檬水找零（六十六）"
 	},
 	{
-		"titie": "JavaScript版LeetCode题解，你值得拥有",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105714458",
-		"visit": "382",
-		"discuss": "1"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103970745",
+		"title": "LeetCode题解=>771.宝石与石头（六十五）"
 	},
 	{
-		"titie": "LeetCode题解=>70.爬楼梯",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105710483",
-		"visit": "71",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103962770",
+		"title": "LeetCode题解=>709.转换成小写字母（六十四）"
 	},
 	{
-		"titie": "LeetCode题解=>83.删除排序链表中的重复元素",
-		"link": "https://blog.csdn.net/jbj6568839z/article/details/105701356",
-		"visit": "89",
-		"discuss": "0"
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103954698",
+		"title": "LeetCode题解=>645.错误的集合（六十三）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103954435",
+		"title": "LeetCode题解=>595.大的国家（六十二）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103879679",
+		"title": "LeetCode题解=>509.斐波那契数（六十一）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103876938",
+		"title": "LeetCode题解=>448.找到所有数组中消失的数字（六十）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103876262",
+		"title": "LeetCode题解=>442.数组中重复的数据（五十九）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103872997",
+		"title": "LeetCode题解=>412.FizzBuzz（五十八）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103871988",
+		"title": "LeetCode题解=>371.两整数之和（五十七）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103871523",
+		"title": "LeetCode题解=>344.反转字符串（五十六）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103862896",
+		"title": "LeetCode题解=>287.寻找重复数（五十五）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103807327",
+		"title": "LeetCode题解=>283.移动零（五十四）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103808459",
+		"title": "算法与数据结构+LeetCode题解-Js版"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103671011",
+		"title": "LeetCode-242.有效的字母异位词（三十七）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103806837",
+		"title": "LeetCode题解：237.删除链表中的节点"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103804946",
+		"title": "LeetCode题解=>219.存在重复元素II（五十二）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103803945",
+		"title": "LeetCode题解=>215.数组中的第K个最大元素（五十一）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103785917",
+		"title": "LeetCode题解=>204.计算质数（五十）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103785419",
+		"title": "LeetCode题解=>202.快乐数（四十九）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103783469",
+		"title": "LeetCode题解=>196.删除重复的电子邮箱（四十八）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103782092",
+		"title": "LeetCode题解=>182.查找重复的电子邮箱（四十七）"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103778465",
+		"title": "LeetCode题解=>167.两数之和II-输入有序数组(四十六)"
+	},
+	{
+		"link": "https://blog.csdn.net/jbj6568839z/article/details/103771359",
+		"title": "LeetCode题解=>141.环形链表(四十五)"
 	}
 ]
-
-
--- node/nodetest.js
+++ node/nodetest.js
@@ -1,9 +1,3 @@
-/*
- * @desc:
- * @Author: 余光
- * @Email: webbj97@163.com
- * @Date: 2020-07-10 11:45:48
- */
 var http = require("http");
 var axios = require('axios');
 var cheerio = require('cheerio');
@@ -20,25 +14,21 @@ const fs = require('fs');
 //     response.end('Hello World\n');
 // }).listen(8888);
 
-var baseUrl = 'https://blog.csdn.net/jbj6568839z/article/list/';
+var baseUrl = 'https://blog.csdn.net/jbj6568839z/category_9292818_2.html'; // 专栏
 var list = []
-var func = function(id) {
-    const nowUrl = baseUrl + id
-    axios.get(nowUrl).then(res => {
+var func = function() {
+    axios.get(baseUrl).then(res => {
         var $ = cheerio.load(res.data);
-        var articleList = $('.article-item-box'); // 拿到文章列表
+        var articleList = $('.column_article_list li'); // 拿到文章列表
         var len = articleList.length;
-
         for (let i = 0; i < len; i++) {
-            var now = $(articleList[i]).find('h4 a');
-            var visit = $(articleList[i]).find('.read-num')[0].children[1].data;
-            var discuss = $(articleList[i]).find('.read-num')[1].children[1].data
-            list.push({
-                titie: now.text().replace(/(\s+|原创|原力计划)/g, ''), // 结构原因，需要剔除不需要的部分
+            var now = $(articleList[i]).find('a');
+            var title = now.find('.title');
+            var obj = {
                 link: now.attr('href'),
-                visit,
-                discuss
-            })
+                title: title.text().replace(/(\s+|原创|原力计划)/g, '')
+            }
+            list.push(obj)
         }
 
         let str = JSON.stringify(list, null, "\t")
@@ -50,5 +40,36 @@ var func = function(id) {
         console.log(err);
     })
 }
+func()
 
-func(1)
+
+// var func = function(id) {
+//     const nowUrl = baseUrl + id
+//     axios.get(nowUrl).then(res => {
+//         var $ = cheerio.load(res.data);
+//         var articleList = $('.article-item-box'); // 拿到文章列表
+//         var len = articleList.length;
+
+//         for (let i = 0; i < len; i++) {
+//             var now = $(articleList[i]).find('h4 a');
+//             var visit = $(articleList[i]).find('.read-num')[0].children[1].data;
+//             var discuss = $(articleList[i]).find('.read-num')[1].children[1].data
+//             list.push({
+//                 titie: now.text().replace(/(\s+|原创|原力计划)/g, ''), // 结构原因，需要剔除不需要的部分
+//                 link: now.attr('href'),
+//                 visit,
+//                 discuss
+//             })
+//         }
+
+//         let str = JSON.stringify(list, null, "\t")
+//         fs.writeFile('csdn-data.js', str, { flag: 'a' }, function(err) {
+//             if (err) console.log('error!!!');
+//             console.log('写入完成');
+//         })
+//     }).catch(err => {
+//         console.log(err);
+//     })
+// }
+
+// func(1)
