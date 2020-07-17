56b37bbee24862f15a7461d1d4a7e9887bb4cdfe
yuguang <yuguang@gaoding.com>
2020-07-17 10:08:52 +0800

    Updates

--- Js专题/index.js
+++ Js专题/index.js
@@ -1,8 +1,4 @@
-/*
- * @desc:
- * @Author: 余光
- * @Email: webbj97@163.com
- * @Date: 2019-11-22 10:20:48
- */
-var str = '123';
-console.log(str); //
+function foo(a, b) {
+    console.log('a', a)
+}
+foo();
